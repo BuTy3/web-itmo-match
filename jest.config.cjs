70dbce0e8a60fff7dc08e19b5c9abc8882d7aea4
red <368799@niuitmo.ru>
2025-10-18 00:07:24 +0300

    fix: fix jest config

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,10 +2,14 @@ module.exports = {
   testEnvironment: "jsdom",
   transform: { "^.+\\.[jt]sx?$": "babel-jest" },
   moduleNameMapper: { "\\.(css|scss)$": "identity-obj-proxy" },
+
+  testMatch: ["<rootDir>/src/**/*.test.[jt]s?(x)"],
+  testPathIgnorePatterns: ["/node_modules/", "/tests-e2e/"],
+
   collectCoverage: true,
   collectCoverageFrom: ["src/**/*.{js,jsx}", "!src/**/index.{js,jsx}"],
   coverageReporters: ["text", "lcov"],
   coverageThreshold: {
-    global: { lines: 80, statements: 80, functions: 80, branches: 80 },
+    global: { lines: 80, statements: 80, functions: 80, branches: 80 }, // G3.5
   },
 };
