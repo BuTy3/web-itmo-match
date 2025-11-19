b063ec9638e0375dbbcb6d9dbaf4877b9ae15fdc
second <phamdangtrungnghia@gmail.com>
2025-11-19 19:26:19 +0300

    refactor: reset all demo structure

--- nodejs-backend/services/proxy.js
+++
@@ -1,9 +0,0 @@
-const httpProxy = require("http-proxy-middleware");
-
-const proxyToSpring = httpProxy.createProxyMiddleware({
-  target: "http://localhost:8080", // Spring Boot run port 8080
-  changeOrigin: true,
-  pathRewrite: { "^/api": "" }, // /api/user → http://localhost:8080/user
-});
-
-module.exports = proxyToSpring;
