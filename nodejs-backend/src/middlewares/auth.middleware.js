1fd52f17b65180b6a3b77ca520505482ae9f5de8
second <phamdangtrungnghia@gmail.com>
2025-11-19 20:27:39 +0300

    feat: create middleware for authentication

---
+++ nodejs-backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,51 @@
+import { verifyToken } from '../security/jwt.js';
+
+// API Middleware (return JSON)
+export function authApiRequired(req, res, next) {
+  const authHeader = req.headers['authorization'];
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ ok: false, message: 'Unauthorized' });
+  }
+
+  const token = authHeader.slice(7); // bỏ "Bearer "
+
+  try {
+    const payload = verifyToken(token);
+
+    // Lưu thông tin user vào req
+    req.user = {
+      id: payload.userId,
+      login: payload.login,
+      ukey: payload.ukey,
+    };
+
+    next();
+  } catch (err) {
+    console.error('JWT error:', err.message);
+    return res.status(401).json({ ok: false, message: 'Invalid token' });
+  }
+}
+
+// Page middleware (redirect /login)
+export function authPageRequired(req, res, next) {
+  const authHeader = req.headers['authorization'];
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.redirect('/login');
+  }
+
+  const token = authHeader.slice(7);
+
+  try {
+    const payload = verifyToken(token);
+    req.user = {
+      id: payload.userId,
+      login: payload.login,
+      ukey: payload.ukey,
+    };
+    next();
+  } catch (err) {
+    return res.redirect('/login');
+  }
+}
